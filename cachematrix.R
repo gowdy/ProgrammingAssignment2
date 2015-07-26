10346553c8a015439dcae8e977bccfa03ac57057
Stephen Gowdy <sgowdy@gmail.com>
2015-07-26 19:12:07 +0100

    comment code

--- cachematrix.R
+++ cachematrix.R
@@ -3,8 +3,10 @@
 ## Special matrix object that is cached
 
 makeCacheMatrix <- function(x = matrix()) {
+  ## Cache the inverse at i
   i <- NULL
   set <- function( y ) {
+    ## If we set the actual object invalidate the cache
     x <<- y
     i <<- NULL
   }
@@ -19,11 +21,14 @@ makeCacheMatrix <- function(x = matrix()) {
 ## Invert the matrix from above if needed, use cache if available
 
 cacheSolve <- function(x, ...) {
+  ## Check to see if the inverse has been calculated and cached
   i <- x$getinverse()
   if ( !is.null(i) ) {
+    ## If it has return it
     message("getting cached data")
     return( i )
   }
+  ## if not, calculate the inverse and have it cached
   data <- x$get()
   i <- solve( data, ... )
   x$setinverse( i )
